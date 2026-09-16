bee7f3245334f696c190e4f6798edadb63ef8bd5
DOUMA008 <hugonunes.vitor11@gmail.com>
2026-09-16 10:55:06 -0300

    Atualiza código

--- num = 5.py
+++ num = 5.py
@@ -1,7 +1,19 @@
-def main():
-	num = 5
-	print(num)
-
-
-if __name__ == "__main__":
-	main()
+while True:
+    print("====Calculadora====")
+    print("1. Suma")
+    print("2. subtração") 
+    print("3. divisão")
+    print("4. multiplicação")
+    print("5. sair")
+    print("====================") 
+    
+    opcao = input("Escolha uma opção: ")
+    if opcao == "5":
+        print("Saindo da calculadora...")
+        break
+    
+    if opcao == "1":
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+        resultado = num1 + num2
+        print("O resultado da soma é:", resultado)
